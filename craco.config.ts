b77f8e5979d02b84b384af4eef6056e909079e9f
david <david@localhost.localdomain>
2022-07-17 21:19:52 +0200

    Fix [DEP_WEBPACK_DEV_SERVER_ON_AFTER_SETUP_MIDDLEWARE]

--- craco.config.ts
+++ craco.config.ts
@@ -1,1 +1,46 @@
-export {}
+import fs from "fs";
+
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+import evalSourceMap from "react-dev-utils/evalSourceMapMiddleware";
+import redirectServedPath from "react-dev-utils/redirectServedPathMiddleware";
+import noopServiceWorker from "react-dev-utils/noopServiceWorkerMiddleware";
+
+import type { CracoConfig } from "@craco/craco";
+
+const config: CracoConfig = {
+  webpack: {
+    plugins: {
+      add: [
+        new NodePolyfillPlugin({
+          excludeAliases: ["console"],
+        }),
+      ] /* An array of plugins */,
+    },
+  },
+  devServer: (devServerConfig, { env, paths }) => {
+    devServerConfig = {
+      onBeforeSetupMiddleware: undefined,
+      onAfterSetupMiddleware: undefined,
+      setupMiddlewares: (middlewares, devServer) => {
+        if (!devServer) {
+          throw new Error("webpack-dev-server is not defined");
+        }
+
+        if (fs.existsSync(paths.proxySetup)) {
+          require(paths.proxySetup)(devServer.app);
+        }
+
+        middlewares.push(
+          evalSourceMap(devServer),
+          redirectServedPath(paths.publicUrlOrPath),
+          noopServiceWorker(paths.publicUrlOrPath)
+        );
+
+        return middlewares;
+      },
+    };
+    return devServerConfig;
+  },
+};
+
+export default config;
